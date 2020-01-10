ad4f89b5e1b539af4e79a5801414b05825a0f318
Gautam Sisodia <gautam.sisodia@gmail.com>
2020-01-10 21:14:02 +0000

    added functions to pull temperatures from GHCN dly files

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # climate
 Exploring climate data
 
+## Global Historical Climatology Network (GHCN)
+https://www.ncdc.noaa.gov/data-access/land-based-station-data/land-based-datasets/global-historical-climatology-network-ghcn
+
+## To do
+- [ ] check flags in ghcn data
+- [ ] read https://www.nature.com/articles/s41558-019-0666-7
--- src/ghcn.py
+++ src/ghcn.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 ghcn_specs = [
     {
-        "name": "stations",
+        "file_type": "stations",
         "file_name": "ghcnd-stations.txt",
         "desc": "stations",
         "field_specs": [
@@ -20,12 +20,31 @@ ghcn_specs = [
             {"field_name": "WMO ID", "pos_from": 81, "pos_to": 85},
         ],
     },
-    {"name": "readme", "file_name": "readme.txt", "desc": "readme"},
+    {"file_type": "readme", "file_name": "readme.txt", "desc": "readme"},
+    {
+        "file_type": "station_data",
+        "desc": "data for one station (.dly file), see section III of readme",
+        "field_specs": [
+            {"field_name": "ID", "pos_from": 1, "pos_to": 11},
+            {"field_name": "YEAR", "pos_from": 12, "pos_to": 15},
+            {"field_name": "MONTH", "pos_from": 16, "pos_to": 17},
+            {"field_name": "ELEMENT", "pos_from": 18, "pos_to": 21},
+        ]
+        + [
+            {
+                "field_name": "{typ}{day}".format(typ=typ, day=day),
+                "pos_from": 14 + 8 * day + (0 if typ_ind == 0 else typ_ind + 4),
+                "pos_to": 14 + 8 * day + (typ_ind + 4),
+            }
+            for typ_ind, typ in enumerate(["VALUE", "MFLAG", "QFLAG", "SFLAG"])
+            for day in range(1, 32)
+        ],
+    },
 ]
 
 
-def get_ghcn_specs(name):
-    return [g for g in ghcn_specs if g["name"] == name][0]
+def get_ghcn_specs(file_type="stations"):
+    return [g for g in ghcn_specs if g["file_type"] == file_type][0]
 
 
 def get_ghcn_file(file_name: str = "ghcnd-stations.txt"):
@@ -42,17 +61,45 @@ def get_ghcn_file(file_name: str = "ghcnd-stations.txt"):
     return file
 
 
-def get_stations():
-    stations = get_ghcn_file("ghcnd-stations.txt")
-    stations_field_pos = [
+def read_fwf(file_name: str = "ghcnd-stations.txt", file_type: str = "stations"):
+    file = get_ghcn_file(file_name)
+    field_pos = [
         (field["pos_from"] - 1, field["pos_to"])
-        for field in get_ghcn_specs("stations")["field_specs"]
+        for field in get_ghcn_specs(file_type)["field_specs"]
     ]
-    stations_field_names = [
-        field["field_name"] for field in get_ghcn_specs("stations")["field_specs"]
+    field_names = [
+        field["field_name"] for field in get_ghcn_specs(file_type)["field_specs"]
     ]
-    stations_df = pd.read_fwf(
-        StringIO(stations), colspecs=stations_field_pos, header=None
-    )
-    stations_df.columns = stations_field_names
-    return stations_df
+    df = pd.read_fwf(StringIO(file), colspecs=field_pos, header=None)
+    df.columns = field_names
+    return df
+
+
+def get_stations():
+    return read_fwf(file_name="ghcnd-stations.txt", file_type="stations")
+
+
+def get_station_data(station_id: str = "USW00014771"):
+    return read_fwf(file_name="all/{}.dly".format(station_id), file_type="station_data")
+
+
+def c_to_f(c):
+    """
+    tenths of degrees Celsius to Fahrenheit
+    """
+    return (c * 9.0 / 50.0) + 32
+
+
+def get_temps(station_data):
+    temps = pd.melt(
+        frame=station_data[station_data.ELEMENT.isin(["TMAX", "TMIN"])],
+        id_vars=["ID", "YEAR", "MONTH", "ELEMENT"],
+        value_vars=["VALUE{}".format(day) for day in range(1, 32)],
+        var_name="var",
+        value_name="temp_c",
+    ).query("temp_c != -9999")
+
+    temps["day"] = temps["var"].map(lambda x: x[5:])
+    temps["temp_f"] = temps["temp_c"].map(c_to_f)
+    temps["date"] = pd.to_datetime(temps[["YEAR", "MONTH", "day"]])
+    return temps
