2d6c0ba2f15efc4a4fea6ef3af8a0da02946fee9
Gautam Sisodia <gautam.sisodia@gmail.com>
2020-11-08 23:34:54 -0500

    Update documentation

--- _sources/notebooks/nyiso_explore.py
+++ _sources/notebooks/nyiso_explore.py
@@ -4,11 +4,36 @@
 Exploring NYISO data, in particular electric consumption fuel mix over time
 """
 
-# %% [hide-cell]
+# %% tags=['hide-cell']
 from IPython import get_ipython
 if get_ipython() is not None:
     get_ipython().run_line_magic('load_ext', 'autoreload')
     get_ipython().run_line_magic('autoreload', '2')
 
 # %%
+from climate.nyiso import utils as u
+import logging
+from pygsutils import general as g
 
+# %%
+dfp = "./../data"
+
+log_fp = "./../logs/get_nyiso.log"
+
+# %%
+g.setup_logging(log_fp)
+
+
+# %%
+fm = u.get_fuel_mix(data_folder_path=dfp)
+
+# %%
+len(fm)
+
+# %%
+fm.info()
+
+# %%
+fm.head()
+
+# %%
